59ccda66bb5ef2b6129d2c5c4c01de19ae5d0b26
Kthateyo <kthateyo@gmail.com>
2019-11-11 22:24:18 +0100

    Cleaning and background

---
+++ src/components/Background.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="background"></div>
+</template>
+
+<script>
+export default {
+  name: 'Background',
+};
+</script>
+
+<style>
+.background {
+  width: 100%;
+  height: 100vh;
+
+  position: fixed;
+  z-index: -1;
+  background: url('https://images.pexels.com/photos/1252869/pexels-photo-1252869.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260') no-repeat;
+  background-size: cover;
+}
+</style>
