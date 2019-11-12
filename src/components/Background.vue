b8ac21765c9166611936102c7abe605e7716589c
Kthateyo <kthateyo@gmail.com>
2019-11-12 18:28:20 +0100

    Fix styles in Background.vue

--- src/components/Background.vue
+++ src/components/Background.vue
@@ -8,13 +8,16 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .background {
   width: 100%;
   height: 100vh;
 
   position: fixed;
+  top: 0;
+  left: 0;
   z-index: -1;
+
   background: url('https://images.pexels.com/photos/1252869/pexels-photo-1252869.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260') no-repeat;
   background-size: cover;
 }
