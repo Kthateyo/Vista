a3c13c0ec953195b92e4a472580088fc76d2c2cf
Kthateyo <kthateyo@gmail.com>
2019-11-12 18:28:30 +0100

    Add Claim.vue

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,19 @@
   <div id="app">
 
     <Background/>
-
+    <Claim/>
   </div>
 </template>
 
 <script>
 import Background from './components/Background.vue';
+import Claim from './components/Claim.vue';
 
 export default {
   name: 'app',
   components: {
     Background,
+    Claim,
   },
 };
 </script>
@@ -21,11 +23,13 @@ export default {
 * {
   margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
 
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: azure;
 }
 </style>
---
+++ src/components/Claim.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="claim">
+    <div class="title">VISTA</div>
+    <div class="description">
+      Our site provides you tons of high quality images of things you like.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Claim',
+};
+</script>
+
+<style scoped>
+.claim {
+  width: fit-content;
+
+  margin: auto;
+  margin-top: 30vh;
+  transform: translateY(-50%);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+
+}
+
+.title {
+  margin-bottom: 20px;
+
+  font-size: 2.2em;
+  font-weight: 600;
+}
+
+.description {
+  font-size: 1.2em;
+  text-align: center;
+}
+
+</style>
